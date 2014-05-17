db2442ad59b584b7635a40498b770fe0a554847a
Ryan Sorensen <rcsorensen@gmail.com>
2014-05-17 14:45:16 -0700

    remove unnecessary deps

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 var jshintLoader = require("jshint-loader");
 var jsxLoader = require("jsx-loader");
-var loaderUtils = require("loader-utils");
-
-var jsxTransform = require('react-tools').transform;
 
 module.exports = function(input) {
   return jshintLoader.call(
     this,
-    jsxLoader.call(this, transformedSource)
+    jsxLoader.call(this, input)
   );
 }
