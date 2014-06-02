5c9922a2c33e8dbd6da7c6492557671555c6953c
Ryan Sorensen <rcsorensen@gmail.com>
2014-06-01 19:51:02 -0700

    Don't allow jsx loader to modify the return value

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ var jshintLoader = require("jshint-loader");
 var jsxLoader = require("jsx-loader");
 
 module.exports = function(input) {
-  return jshintLoader.call(
+  jshintLoader.call(
     this,
     jsxLoader.call(this, input)
   );
+  return input;
 }
